e297abb29bffdfcaef95f9392b55f3c99ea75be5
Anton Rychkov <antrychkov@gmail.com>
2020-05-20 10:27:58 +0500

    add test for geolocation

---
+++ test/test_devices.js
@@ -0,0 +1,36 @@
+const { webkit, devices } = require('playwright');
+const iPhone11 = devices['iPhone 11 Pro'];
+const expect = require('chai').expect;
+
+let browser;
+let page;
+
+before(async () => {
+  browser = await webkit.launch();
+  context = await browser.newContext({
+    ...iPhone11,
+    geolocation: { longitude: 60.574802, latitude: 56.832554 },
+    permissions: ['geolocation']
+  });
+});
+
+after(async () => {
+  await browser.close();
+});
+
+beforeEach(async () => {
+  page = await context.newPage();
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe('test permissions', function () {
+  it('is geolocation', async () => {
+    await page.goto('https://yandex.ru/maps');
+    await page.click('.interstitial-view__links-web');
+    const sidebarTitle = await page.$eval('.config-view', e => e.textContent);
+    expect(sidebarTitle).to.have.string('Екатеринбург');
+  });
+});
